42b4762f014df3abd5d8119906ad1803bc2f888c
Ryan J. W. Kim <tamielryan@gmail.com>
2022-06-25 22:34:00 +0300

    Initial Commit

---
+++ HotdogOrNot/HotdogOrNotApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotdogOrNotApp.swift
+//  HotdogOrNot
+//
+//  Created by Ryan J. W. Kim on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HotdogOrNotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
